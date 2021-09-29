92b241d97d43bbdb7ec0d88a20fc366d3f9892c8
maybeep <60990725+maybeep@users.noreply.github.com>
2021-09-29 01:10:30 -0400

    *successfully* added bootstrap to project

--- src/App.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import './App.css';  
-import Layout  from './Layout/Layout'
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row } from 'react-bootstrap';
+
+function App(): JSX.Element {
+  return (
+    <Container className="App">
+      <Row>
+        <h1>MINERALS</h1>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;

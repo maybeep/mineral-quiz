2961fddc7939c358712adba8e55edc9debec9cc6
maybeep <60990725+maybeep@users.noreply.github.com>
2021-09-29 01:34:04 -0400

    not working at the moment but trying to add question bank

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Menu } from './components/Menu';
 import { Container, Row } from 'react-bootstrap';
 
 function App(): JSX.Element {
   return (
     <Container className="App">
       <Row>
-        <h1>MINERALS</h1>
+        <Menu>
+          
+        </Menu>
+      </Row>
+      <Row>
+        <h1>Mineral #</h1>
       </Row>
     </Container>
   );
---
+++ src/components/Menu.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export function Menu ({}:
+    {
+
+    }): JSX.Element {
+
+
+    return <nav className="nav flex-column">
+        <a className="nav-link" href="#">Mineral 1</a>
+        <a className="nav-link" href="#">Mineral 2</a>
+        <a className="nav-link" href="#">Mineral 3</a>
+        <a className="nav-link" href="#">Mineral 4</a>
+    </nav>
+    }
+
